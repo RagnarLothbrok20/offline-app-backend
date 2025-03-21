3bf4f234477bc8ab30d8364a0a1ffb45be5c1e75
Viswa <viswa@Viswas-MacBook-Pro.local>
2025-03-21 21:45:48 +0530

    URL change 1

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({ origin: "*" })); // Allow all origins
 mongoose
-  .connect('mongodb+srv://aayirathiloruvan20:viswaDbPwd@cluster0.vxtrh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+  .connect('mongodb+srv://aayirathiloruvan20:viswaDbPwd@cluster0.vxtrh.mongodb.net/offlineApp?retryWrites=true&w=majority&appName=Cluster0')
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.error('MongoDB Connection Error:', err));
 
